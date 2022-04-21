5f64cb107d0bf01df8fa0678a3b8b15df8574782
KarlrDing <karl_rding@126.com>
2022-04-21 03:29:39 +0100

    annotate main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ sys.path.append('./Stage_II')
 import data_preprocess
 import model_stage1
 import model_stage2
-
+# ======================================================================================================================
+# Data preprocessing
+# ======================================================================================================================
 #import data
 raw_data = data_preprocess.info()
 #preprocess
@@ -28,12 +30,17 @@ data_preprocess.dist_minor(bal_s1,2)
 data_preprocess.dist_minor(ori_s2,3)
 data_preprocess.dist_minor(bal_s2,4)
 
+# ======================================================================================================================
+# Stage-I
+# ======================================================================================================================
 #Stage-I model and trainning
 model_stage1.model_dataset_choose(0,ori_s1,bal_s1)
 model_stage1.model_dataset_choose(1,ori_s1,bal_s1)
 model_stage1.model_dataset_choose(3,ori_s1,bal_s1)
 model_stage1.model_dataset_choose(4,ori_s1,bal_s1)
-
+# ======================================================================================================================
+# Stage-II
+# ======================================================================================================================
 #Stage-II model and trainning
 model_stage2.model_dataset_choose(0,ori_s2,bal_s2)
 model_stage2.model_dataset_choose(1,ori_s2,bal_s2)
